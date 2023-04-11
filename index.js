e7a9c4cd61cf73f643caa189b274bec615b38eaa
Robyn Drake <robin.drake.041@gmail.com>
2023-04-11 14:19:24 -0500

    backed up a few steps, now actually have data

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import * as cheerio from 'cheerio';
 
 const url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
 
-const $ = cheerio.load(url);
-//console.log($);
+const response = await fetch(url);
+const body = await response.text();
+
+console.log(body);
+
+
+/*
 let data = $("mw-page-container-inner").html();
 
 console.log(data);
+*/
