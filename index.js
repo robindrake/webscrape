452fc11b35f161a1cb05ed97ee1709fcdc038ddf
Robyn Drake <robin.drake.041@gmail.com>
2023-04-11 13:41:01 -0500

    Interim commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
 
 const url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
 
+const $ = cheerio.load(url);
+
 const response = await fetch(url);
 const body = await response.text();
+let data = $("big > a", body);
 
-console.log(body);
+console.log(data);
