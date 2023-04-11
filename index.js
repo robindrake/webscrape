5732858497e4ea9462d7bad83cb36dd8c8956943
Robyn Drake <robin.drake.041@gmail.com>
2023-04-11 14:05:44 -0500

    Not sure what is going on, no data being found

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import * as cheerio from 'cheerio';
 const url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
 
 const $ = cheerio.load(url);
-
-const response = await fetch(url);
-const body = await response.text();
-let data = $("big > a", body);
+//console.log($);
+let data = $("mw-page-container-inner").html();
 
 console.log(data);
